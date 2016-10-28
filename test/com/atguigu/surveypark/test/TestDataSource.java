c339310f04e7bd105092770eb14807d3fb68b2e9
Tralo <13580548169@163.com>
2016-10-28 22:03:31 +0800

    数据源测试链接

---
+++ test/com/atguigu/surveypark/test/TestDataSource.java
@@ -0,0 +1,23 @@
+package com.atguigu.surveypark.test;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 时数据源
+ */
+public class TestDataSource {
+	
+	@Test
+	public void getConnection() throws SQLException{
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		DataSource ds = (DataSource) ac.getBean("dataSource");
+		System.out.println(ds.getConnection());
+	}
+
+}
